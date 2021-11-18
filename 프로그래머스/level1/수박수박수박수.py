ba0c27b0b681983bb5a085ad68cd1a30127f687f
Yunjaejo <1a616@naver.com>
2021-11-18 10:59:39 +0900

    211117 수박수박수박수 주석추가, 코드정리

--- 프로그래머스/level1/수박수박수박수.py
+++ 프로그래머스/level1/수박수박수박수.py
@@ -1,8 +1,7 @@
 def solution(n):
-    if n == 1 :
-        return '수'
-    elif n % 2 == 0:
-        answer = '수박' * int(n/2)
-        return answer
-    elif n % 2 == 1:
-        return '수' + ('박수' * int(n//2))
+    if n % 2 == 0:  # --- n이 짝수라면
+        answer = '수박' * int(n/2)  # --- 수박이라는 두글자에 n의 절반만큼 곱해서 출력
+    else:  # --- n이 홀수라면
+        answer = '수' + ('박수' * (n//2))  # --- 수 라는 글자에 박수를 n의 절반으로 나눈 몫만큼 반복해서 더함.
+
+    return answer
