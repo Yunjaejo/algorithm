305063885af29b678fbbe19ee23fad6ddd176ffe
Yunjaejo <1a616@naver.com>
2021-11-18 10:54:52 +0900

    211117 나누어 떨어지는 숫자배열 주석추가

--- 프로그래머스/level1/나누어떨어지는숫자배열.py
+++ 프로그래머스/level1/나누어떨어지는숫자배열.py
@@ -1,10 +1,10 @@
 def solution(arr, divisor):
     answer = []
     for i in arr:
-        if i % divisor == 0:
+        if i % divisor == 0:  # --- 나누어 떨어진다면 정답배열에 추가
             answer.append(i)
             answer.sort()
-    if len(answer) == 0:
+    if len(answer) == 0:  # --- 정답배열에 하나도 없다면 -1을 리턴
         answer.append(-1)
 
     return answer
