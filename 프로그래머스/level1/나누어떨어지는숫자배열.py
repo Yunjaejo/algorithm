16756f3a9f0751ef3b362542e1797d0e23ab80bc
Yunjaejo <1a616@naver.com>
2021-11-19 11:28:06 +0900

    코드 수정

--- 프로그래머스/level1/나누어떨어지는숫자배열.py
+++ 프로그래머스/level1/나누어떨어지는숫자배열.py
@@ -3,7 +3,7 @@ def solution(arr, divisor):
     for i in arr:
         if i % divisor == 0:  # --- 나누어 떨어진다면 정답배열에 추가
             answer.append(i)
-            answer.sort()
+    answer.sort()
     if len(answer) == 0:  # --- 정답배열에 하나도 없다면 -1을 리턴
         answer.append(-1)
 
--- 프로그래머스/level1/하샤드수.py
+++ 프로그래머스/level1/하샤드수.py
@@ -1,4 +1,4 @@
 def solution(x):
     xToArr = map(int, list(str(x)))  # --- 인자 x를 리스트로 바꾸기 위해 문자열로 타입변경 후 리스트화시킨 뒤 각 항을 숫자로 다시 변경,
     sumX = sum(xToArr)  # --- 각 항이 숫자이니 모두 더한다.
-    return True if x % sumX == 0 else False  # --- 파이썬의 삼항연산자. x % sumX === 0 ? true : false 와 같다
+    return x % sumX == 0  # --- 파이썬의 삼항연산자. x % sumX === 0 ? true : false 와 같다
