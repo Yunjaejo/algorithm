9a98e9153e629ca6e64d4aa84e90559690b97b2f
Yunjaejo <1a616@naver.com>
2021-12-07 17:24:00 +0900

    211207 주석정리

--- 백준/달팽이는올라가고싶다(2869).py
+++ 백준/달팽이는올라가고싶다(2869).py
@@ -2,9 +2,13 @@ A, B, V = map(int, input().split())
 
 day = (V - B) / (A - B)
 
-if day % 1 > 0:
+if day % 1 > 0:  # 만약 day 가 정확히 나누어 떨어지지 않는다면 (4.2일이 걸린다면) 5일만에 올라간 것이므로 +1
     print(int(day) + 1)
 else:
     print(int(day))
+
+# V = (A-B) * day + B 임. (마지막날의 -B까지 계산했기에 하루치의 B를 더해줌)
+# ->  day(V-B)/(A-B)
+
 
 
--- 백준/더하기사이클(1110).py
+++ 백준/더하기사이클(1110).py
@@ -3,19 +3,19 @@ cnt = 0
 origin = n
 
 
-def sum_n(n):
-    return sum(map(int, (list(str(n)))))
+def sum_n(n):  # 두 자리 수의 각 자리의 합을 구하는 함수
+    return sum(map(int, (list(str(n)))))  # 정수 -> 문자열 -> 배열 -> 정수의 배열 -> 합
 
 
-def get_one(n):
-    list_n = list(str(n))[-1:]
-    return int(list_n[0])
+def get_one(n):  # 일의자리의 수를 구하는 함수
+    list_n = list(str(n))[-1:]  # 정수 -> 문자열 -> 배열 -> 마지막자리 찾기
+    return int(list_n[0])  # 마지막 자리 수(문자)를 정수로 변경
 
 
 while True:
-    n = get_one(n) * 10 + get_one(sum_n(n))
-    cnt += 1
-    if n == origin:
+    n = get_one(n) * 10 + get_one(sum_n(n))  # n의 일의자리 x 10 + 합의 일의자리
+    cnt += 1  # 더하고 카운트 + 1
+    if n == origin:  # 더한 값이 처음과 같다면 중지
         break
 
 print(cnt)
