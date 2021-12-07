2689c303f11e439c792a878d516c3df83d624e31
Yunjaejo <1a616@naver.com>
2021-12-08 00:36:58 +0900

    211208 코드수정

--- 백준/터렛(1002).py
+++ 백준/터렛(1002).py
@@ -1,7 +1,7 @@
 case = int(input())
 for _ in range(case):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    point_range = (abs(x1 - x2) * abs(x1 - x2) + abs(y1 - y2) * abs(y1 - y2)) ** 0.5
+    point_range = (abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2) ** 0.5
     if point_range == 0 and r1 == r2:
         print(-1)
     elif r1 + r2 == point_range or abs(r1 - r2) == point_range:
