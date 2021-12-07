fb526ae49cf4f67ff0de1d1705d412acf12ebc81
Yunjaejo <1a616@naver.com>
2021-12-07 14:45:13 +0900

    211207

--- 백준/소수구하기(1929).py
+++ 백준/소수구하기(1929).py
@@ -6,10 +6,10 @@ def is_prime(m, n):
     prime = [True] * n  # n개의 True 배열 생성
     for i in range(2, int(n ** 0.5) + 1):  # n의 제곱근 까지만 검사
         if prime[i]:  # prime의 i번째가 트루라면
-            for j in range(2 * i, n, i):  # i의 배수들을 False 로 변환
+            for j in range(i * i, n, i):  # i의 배수들을 False 로 변환
                 prime[j] = False
     for i in range(m, n):
-        if i > 1 and prime[i] == True:  # 1 이상이면서 남은 소수들
+        if i > 1 and prime[i]:  # 1 이상이면서 남은 소수들
             print(i)
 
 
